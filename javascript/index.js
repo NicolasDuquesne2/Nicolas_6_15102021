73a4e1bdeb72aeae539f052eaa1dfd677b0c74bc
NicolasDuquesne2 <nicolas.duquesne@caramail.fr>
2021-10-30 16:02:45 +0200

    adding in media pics, vids. style is enable for main page. javascript is in build

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,50 @@
 /* At loading page */
 
-function display(datas) {
-  console.log(datas);
+/* merges arrays from objects and returns an array with unique values */
+function mergeArraysAndGetUnique(objects) {
+  let uniqueArray = [];
+  objects.forEach((element) => {
+    const tagsArray = element.tags;
+    uniqueArray = Array.from(new Set(uniqueArray.concat(tagsArray)));
+  });
+  return uniqueArray;
 }
 
-async function getdatas() {
+/* printTags displays tags model in the html with datas */
+function printTags(objects) {
+  const tagHtmlUl = document.querySelector('#main-tags-list');
+  let tagHtmlModel = '';
+  objects.forEach((element) => {
+    tagHtmlModel += `<li><a href="index.html" class="main-tag-link"><span aria-label="${element}">#${element}</span></a></li>`;
+  });
+  tagHtmlUl.innerHTML = tagHtmlModel;
+}
+
+/* lauches the display process according type given */
+function printObjects(objects, type) {
+  switch (type) {
+    case 'tags':
+      printTags(objects);
+      break;
+    case 'photographers':
+      //printPhotographers(objects);
+      break;
+    default:
+  }
+}
+
+async function displayDynamics(url) {
   try {
-    const response = await fetch('./db/photographers.json');
-    const datas = await response.json();
-    display(datas);
+    const response = await fetch(url);
+    const objects = await response.json();
+    const tags = mergeArraysAndGetUnique(objects.photographers);
+    printObjects(tags, 'tags');
+    //printObjects(objects.photographers, 'photographers');
   } catch (error) {
     alert(error.message);
   }
   return true;
 }
 
-getdatas();
-console.log("LUL");
+displayDynamics('./db/photographers.json');
+console.log('LUL');
